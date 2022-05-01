d13af8a22cac779127892d05b05d78812b667aad
Paul_Liao_1457 <85085555+pp1457@users.noreply.github.com>
2022-05-01 20:50:17 +0800

    Create compiler.php

---
+++ IDE/app/compiler.php
@@ -0,0 +1,24 @@
+<?php
+    $language = strtolower($_POST['language']);
+    $code = $_POST['code'];
+    $input = $_POST['file'];
+
+    $random = substr(md5(mt_rand()), 0, 7);
+    $filePath = "temp/" . $random . "." . $language;
+    $programFile = fopen($filePath, "w");
+    fwrite($programFile, $code);
+    fclose($programFile);
+
+    if($language == "cpp") {
+        $outputExe = $random . ".exe";
+        shell_exec("g++ $filePath -o $outputExe");
+        $output = shell_exec(__DIR__ . "//$outputExe < $input ");
+        echo($output);
+    }
+
+    if($language == "c") {
+        $outputExe = $random . ".exe";
+        shell_exec("gcc $filePath -o $outputExe");
+        $output = shell_exec(__DIR__ . "//$outputExe");
+        echo($output);
+    }
