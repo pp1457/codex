a0b6b31d20261f5d04c4d92a838bf7891763d621
Paul_Liao_1457 <85085555+pp1457@users.noreply.github.com>
2022-05-22 21:15:21 +0800

    Update compiler.php

--- IDE/app/compiler.php
+++ IDE/app/compiler.php
@@ -1,9 +1,9 @@
 <?php
     $language = strtolower($_POST['language']);
     $code = $_POST['code'];
-    $input = $_POST['file'];
+    $problemID = (int) $_POST['problemID'];
 
-    $random = substr(md5(mt_rand()), 0, 7);
+    $random = "always";
     $filePath = "temp/" . $random . "." . $language;
     $programFile = fopen($filePath, "w");
     fwrite($programFile, $code);
@@ -12,13 +12,33 @@
     if($language == "cpp") {
         $outputExe = $random . ".exe";
         shell_exec("g++ $filePath -o $outputExe");
-        $output = shell_exec(__DIR__ . "//$outputExe < $input ");
+        $output = shell_exec(__DIR__ . "//$outputExe < testCase//$problemID.txt ");
         echo($output);
     }
 
+    /*
     if($language == "c") {
         $outputExe = $random . ".exe";
         shell_exec("gcc $filePath -o $outputExe");
         $output = shell_exec(__DIR__ . "//$outputExe");
         echo($output);
     }
+
+    if($language == "node") {
+        rename($filePath, $filePath.".js");
+        $output = shell_exec("/usr/local/bin/node $filePath.js 2>&1");
+        echo $output;
+    }
+
+    if($language == "python") {
+        $output = shell_exec("/usr/local/bin/python3 $filePath 2>&1");
+        echo $output;
+    }
+
+    if($language == "php") {
+        $output = shell_exec("php $filePath 2>&1");
+        echo $output;
+    }
+    */
+    
+?>
