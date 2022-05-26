25d03f4bda50581597c3879aca5ef70630136663
Paul_Liao_1457 <85085555+pp1457@users.noreply.github.com>
2022-05-26 16:13:43 +0800

    Add files via upload

---
+++ IDE/ui/js/User.js
@@ -0,0 +1,11 @@
+/*$(function() {
+    var current_progress = 0;
+    var interval = setInterval(function() {
+        current_progress += 10;
+        $("#dynamic")
+        .css("width", current_progress + "%" )
+        .attr("aria-valuenow", current_progress);
+        if (current_progress >= 100)
+            clearInterval(interval);
+    }, 1000);
+  });*/
