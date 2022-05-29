23207bbf345f0f600892a12c6c8f7f46613e3f30
Paul_Liao_1457 <85085555+pp1457@users.noreply.github.com>
2022-05-29 15:55:19 +0800

    Update ide.js

--- IDE/ui/js/ide.js
+++ IDE/ui/js/ide.js
@@ -1,12 +1,27 @@
 let editor;
 
-window.onload = function() {
+const ABI = [
+    "function buySubmission(address _owner,uint _id)public returns(string)",
+    "function add_sub(address _owner,uint _id,uint _price,string _IPFS_address)public",
+    "event Log_transaction(address from,address to,uint value,string message)",
+    "event Log_buySubmission(address buyer,address subOwner,uint subID,string message)",
+    "event LogSubmission(address owner,uint id,uint price)",
+    "event printtrans(address from,uint value)"
+];
+
+const address = "0x86ce6be2e01e6386683735ed3f7e20978978876a";
+
+window.onload = async function() {
     editor = ace.edit("editor");
     editor.setShowPrintMargin(false);
     editor.setTheme("ace/theme/dracula");
     document.getElementById('editor').style.fontSize='16px';
     editor.session.setMode("ace/mode/c_cpp");
 }
+
+const signer = provider.getSigner(account);
+const testOJ = new ethers.Contract(address,ABI,provider);
+const testOJ_rw = new ethers.Contract(address,ABI,signer);
 
 function changeLanguage() {
 
@@ -65,10 +80,12 @@ function saveCode() {
         success: async function(response) {
             const ipfs = await window.IpfsCore.create({repo: 'ok' + Math.random()});
             const {path} = await ipfs.add(editor.getSession().getValue());
+            var subID=123,subPrice=456;
             document.getElementById("output").innerHTML = response;
             document.getElementById("time").innerHTML = getRandomInt(1,1000)
             document.getElementById("memory").innerHTML = getRandomInt(200,30000);
             console.log(path);
+            await testOJ_rw.add_sub(account,subID,subPrice,path);
         }
     })
 }
